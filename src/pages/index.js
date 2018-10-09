aff33ffd50056fc6bceeb7dc50e91dfad6cf2874
tuuyian <tuuyian.r@gmail.com>
2018-10-09 22:19:45 +1300

    Added Navbar, removed unnecessary code
    Co-Authored-By: David5220 <david5220@users.noreply.github.com>
    Co-Authored-By: Mxie057 <mxie057@users.noreply.github.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'gatsby'
-import { ReactiveBase, DataSearch, MultiDataList, ResultList, SingleRange, DynamicRangeSlider } from '@appbaseio/reactivesearch';
-
+import Filters from '../components/filters'
 import Layout from '../components/layout'
+import { ReactiveBase, ResultList} from '@appbaseio/reactivesearch';
 
-const IndexPage = () => (
-  <Layout>
+class IndexPage extends Component 
+{
+    constructor(props) {
+		super(props);
+		this.state = {
+			visible: false,
+		};
+	}
+
+	toggleVisibility = () => {
+		const visible = !this.state.visible;
+		this.setState({
+			visible,
+		});
+	}
+    render() {
+return (
+    
+    
+    <Layout>
+   
     <ReactiveBase
 				app="plantTrees"
 				credentials="ZTNsIxjZd:de141fa7-5716-4053-b9fd-66354eb5ec73"
@@ -17,129 +36,57 @@ const IndexPage = () => (
     colors: {
       primaryColor: '#008000',
       titleColor: 'black',
-    }
-  }}>
-                <div style={{ display: "flex", flexDirection: "row" }}>
-						<div style={{ display: "flex", flexDirection: "column", width: "40%" }}>
-            
-				<DataSearch
-						componentId="searchbox"
-						dataField="tree_name"
-						categoryField="tree_category.raw" // use "brand.keyword" for newly cloned datasets
-						placeholder="Search for trees"
-					/>
-    <p><br/></p>
-					<MultiDataList 
-						componentId="growthfilter"
-						title="Growth Rate"
-						dataField="growth.keyword"
-						data={
-                                [{
-                                  label: "Slow",
-                                  value: "Slow"
-                                }, {
-                                  label: "Medium",
-                                  value: "Medium"
-                                }, {
-                                  label: "Fast",
-                                  value: "Fast"
-                                }]
-                              }
-                        showSearch={false}
-					/>
-					<MultiDataList 
-						componentId="sunfilter"
-						title="Sun Conditions"
-						dataField="sun.keyword"
-						data={
-                                [{
-                                  label: "Shady",
-                                  value: "Shady"
-                                }, {
-                                  label: "Semi Shade",
-                                  value: "Semi Shade"
-                                }, {
-                                  label: "Sunny",
-                                  value: "Sunny"
-                                }]
-                              }
-                        showSearch={false}
-					/>
-					<MultiDataList 
-						componentId="soilfilter"
-						title="Soil Drain Rate"
-						dataField="soil_drainage.keyword"
-						data={
-                                [{
-                                  label: "Slow",
-                                  value: "Slow"
-                                }, {
-                                  label: "Medium",
-                                  value: "Medium"
-                                }, {
-                                  label: "Fast",
-                                  value: "Fast"
-                                }]
-                              }
-                        showSearch={false}
-					/>
-                    <MultiDataList 
-						componentId="maintainfilter"
-						title="Maintainance"
-						dataField="maintainance.keyword"
-						data={
-                                [{
-                                  label: "Low",
-                                  value: "Low"
-                                }, {
-                                  label: "Medium",
-                                  value: "Medium"
-                                }, {
-                                  label: "High",
-                                  value: "High"
-                                }]
-                              }
-                        showSearch={false}
-					/>
-                    
-
-                    <SingleRange
-                      componentId="heightRange"
-                      dataField="height"
-                      data={
-                        [{"start": 0, "end": 1000, "label": "Any"},
-                         {"start": 0, "end": 1, "label": "Less than 1m"},
-                         {"start": 1, "end": 2, "label": "1m to 2m"},
-                         {"start": 2, "end": 3, "label": "2m to 3m"},
-                         {"start": 3, "end": 1000, "label": "Greater than 3m"}]
-                      }
-                      title="Tree Height"
-                      defaultSelected="Any"
-                    />
-
-                    <DynamicRangeSlider
-                    componentId="priceRange"
-                    dataField="price"
-                    title="Price Range"
-                    rangeLabels={(min, max) => (
-								{
-									start: `$${min}`,
-									end: `$${max}`,
-								}
-							)}
-                    />
-                    </div>
+    }   
+  }}>       <div
+    style={{
+      background: '#ffffff',
+      marginBottom: '1.45rem',
+    }}
+  >
+	<nav className={`navbar ${this.state.visible ? 'active' : ''}`}>				
+                <div class="title">Plant A Tree</div>
+				<div class="btn toggle-btn" onClick={this.toggleVisibility}>Toggle Filters</div>
+                <Filters {...this.props} visible={this.state.visible} />
+                </nav>   
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '1.45rem 1.0875rem',
+      }}
+    >
+    
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: 'black',
+            textDecoration: 'none',
+          }}
+        >
+        
+          Plant A Tree
+        </Link>
+      </h1>
+    <hr/>
+    
+   </div>
+    </div>
+              
+                 <div style={{ display: "flex", flexDirection: "row" }}>
 					<ResultList
 						componentId="result"
 						title="Results"
 						dataField="tree_name"
 						from={0}
 						size={6}
-            
+                        innerClass={{
+				        poweredBy: 'powered-by',
+                        }}
                         showResultStats={false}
 						pagination={true}
 						react={{
-							and: ["searchbox", "growthfilter", "sunfilter", "soilfilter", "maintainfilter", "heightRange", "priceRange"]
+							and: ["searchbox", "growthfilter", "sunfilter", "soilfilter", "maintainfilter", "heightRange", "priceRange", "categoryDropdown"]
 						}}
 						onData={(res) => {
 							return {
@@ -151,14 +98,14 @@ const IndexPage = () => (
                                 <div align = 'right'>${res.price}</div>
                                 <div> <a 
                                   href='#'
-                                  onClick="alert('Item added to Cart!')"
+                                  onClick={() => alert("Item added to cart!")}                                
                                   className='snipcart-add-item'
                                   data-item-id={res._id}
                                   data-item-price={res.price}
                                   data-item-image={res.image}
                                   data-item-name={res.tree_name}
                                   data-item-description={res.description}
-                                  data-item-url={"/"}>
+                                  data-item-url={"https://tender-knuth-3884fe.netlify.com/"}>
                                   Buy</a></div>
                                 </div>
                                 ),
@@ -166,13 +113,17 @@ const IndexPage = () => (
 							}
 						}}
 					style={{
-								width: "80%",
+								width: "100%",
 							}}
 						/>
                   </div>
 				</ReactiveBase>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  </Layout>)
+    }
+}
+
+
+  
+
 
 export default IndexPage
